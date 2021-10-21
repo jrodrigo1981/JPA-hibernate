819eee268f42bcd53633ffeea31f1e9cabf78c39
jrodrigo1981 <jrodrigo1981@gmail.com>
2021-10-21 15:51:07 -0300

    Corrigido o persistence.xml e criado a entidade Produto

---
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -0,0 +1,89 @@
+package br.com.alura.loja.modelo;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String descricao;
+	private BigDecimal preco;
+
+	public Produto() {
+		super();
+	}
+
+	public Produto(Long id, String name, String descricao, BigDecimal preco) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", name=" + name + ", descricao=" + descricao + ", preco=" + preco + "]";
+	}
+
+}
